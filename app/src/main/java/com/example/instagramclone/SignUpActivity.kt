28cdd63ea52e1e9047257d16554e2ed287ab6d50
Yash <yash9274gautam@gmail.com>
2023-10-20 04:53:42 +0530

    Addded splash screen

---
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    //val binding by lazy
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
