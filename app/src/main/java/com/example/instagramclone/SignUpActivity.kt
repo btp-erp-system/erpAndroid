b098f4f76e12a0fe6d837ba8000dd116ce1c6433
Yash <yash9274gautam@gmail.com>
2023-10-20 14:48:54 +0530

    Validation check on email. It should be iitp mail id.

--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -2,13 +2,36 @@ package com.example.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
 
     //val binding by lazy
+    private lateinit var etEmail: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        etEmail = findViewById(R.id.emailText)
+
+        val btnSignup = findViewById<Button>(R.id.signupbtn)
+        btnSignup.setOnClickListener {
+            val emailPattern = ".*@iitp\\.ac\\.in"
+            val email = etEmail.text.toString().trim()
+            if(email.isEmpty()){
+                etEmail.error = "Email is required"
+                return@setOnClickListener
+            }
+            else if(!email.matches(emailPattern.toRegex())){
+                etEmail.error = "Enter your college email"
+                return@setOnClickListener
+            }
+            else {
+                Toast.makeText(this, "Registration Done", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
