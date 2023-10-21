11550927c71b4d8bc7ee942e659aacb263073f09
Yash <yash9274gautam@gmail.com>
2023-10-22 03:22:46 +0530

    Storing data in firebase and user authentication

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.instagramclone
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +22,28 @@ class MainActivity : AppCompatActivity() {
 
         //window.statusBarColor=Color.TRANSPARENT
 
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "Value is: " + value)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this,SignUpActivity::class.java))
             finish()
         },3000)
     }
+
 }
--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -6,22 +6,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.example.instagramclone.Models.User
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.database
 
 class SignUpActivity : AppCompatActivity() {
 
     //val binding by lazy
     private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var etUserName: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         etEmail = findViewById(R.id.emailText)
+        etPassword = findViewById(R.id.passText)
+        etUserName = findViewById(R.id.usernameText)
+
+        val auth = FirebaseAuth.getInstance()
+        val database = Firebase.database
+
 
         val btnSignup = findViewById<Button>(R.id.signupbtn)
         btnSignup.setOnClickListener {
             val emailPattern = ".*@iitp\\.ac\\.in"
             val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+            val userName = etUserName.text.toString().trim()
             if(email.isEmpty()){
                 etEmail.error = "Email is required"
                 return@setOnClickListener
@@ -30,8 +44,17 @@ class SignUpActivity : AppCompatActivity() {
                 etEmail.error = "Enter your college email"
                 return@setOnClickListener
             }
+            else if(password.length<3){
+                etPassword.error = "Password is to short"
+            }
             else {
                 Toast.makeText(this, "Registration Done", Toast.LENGTH_SHORT).show()
+                auth.createUserWithEmailAndPassword(etEmail.text.toString().trim(),etPassword.text.toString().trim()).
+                addOnCompleteListener {task ->
+                    if(task.isComplete){
+                        val newUser = User(etUserName.text.toString().trim(),etEmail.text.toString().trim(),etPassword.text.toString().trim())
+                    }
+                }
                 startActivity(Intent(this,LoginActivity::class.java))
             }
         }
